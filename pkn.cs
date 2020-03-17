81de3528b70d78a1d26e82fdc13eaccb3b0ee9ae
aleksandra-merchut <aleksandra.merchut1@gmail.com>
2020-03-17 14:08:59 +0000

    modified loop and statement of decision after gameplay

--- pkn.cs
+++ pkn.cs
@@ -2,43 +2,42 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    string figury = "Papier-Kamień-Nożyce";
-    Console.WriteLine ("Witaj w grze "+figury);
+    string figurine = "Papier-Kamień-Nożyce";
+    bool game = true;
+    Console.WriteLine ("Witaj w grze "+figurine);
 
    Console.WriteLine ("Graczu #1, jak masz na imię?");
    string firstPlayerName = Console.ReadLine();
 
    Console.WriteLine ("Graczu #2, jak masz na imię?");
    string secondPlayerName = Console.ReadLine();
-   for(;;){
-   Console.WriteLine ("Graczu " + firstPlayerName + " wybierz jedną z figur: " +figury);
-   Console.WriteLine ("Aby dokonać wyboru wpisz pierwszą literę figury");
-   string firstPlayerChoice = Console.ReadLine();
-   Console.Clear();
-
-   Console.WriteLine ("Graczu " + secondPlayerName + " wybierz jedną z figur: " +figury);
-   Console.WriteLine ("Aby dokonać wyboru wpisz pierwszą literę figury");
-   string secondPlayerChoice = Console.ReadLine();
-   Console.Clear();
-
-   if (firstPlayerChoice.ToUpper() == secondPlayerChoice.ToUpper()) {
-     Console.WriteLine ("REMIS");
-    }else{
-      if((firstPlayerChoice.ToUpper() == "P" && secondPlayerChoice.ToUpper() == "K" ) ||(firstPlayerChoice.ToUpper() == "K" && secondPlayerChoice.ToUpper() == "N" )||(firstPlayerChoice.ToUpper() == "N" && secondPlayerChoice.ToUpper() == "P" ) ){
-        Console.WriteLine ("Gracz " + firstPlayerName + " wygrywa, gratulacje!");
-      } else {
-        Console.WriteLine ("Gracz " + secondPlayerName + " wygrywa, gratulacje!");
-      }
-    }
-    Console.WriteLine ("Czy chcecie zagrać jeszcze raz?\nNapiszcie TAK lub NIE");
-     string playOrNot1 = Console.ReadLine();
-    string playOrNot2 = Console.ReadLine();
-    if (playOrNot1.ToUpper() == "TAK" && playOrNot2.ToUpper() == "TAK"){
-    continue;
-    }else{
-      break;
-   }
-    }
+   while(game==true){   
+    Console.WriteLine ("Graczu " + firstPlayerName + " wybierz jedną z figur: " +figurine);
+    Console.WriteLine ("Aby dokonać wyboru wpisz pierwszą literę figury");
+    string firstPlayerChoice = Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine ("Graczu " + secondPlayerName + " wybierz jedną z figur: " +figurine);
+    Console.WriteLine ("Aby dokonać wyboru wpisz pierwszą literę figury");
+    string secondPlayerChoice = Console.ReadLine();
+    Console.Clear();
+    if (firstPlayerChoice.ToUpper() == secondPlayerChoice.ToUpper()) {
+      Console.WriteLine ("REMIS");
+      }else{
+        if((firstPlayerChoice.ToUpper() == "P" && secondPlayerChoice.ToUpper() == "K" ) ||(firstPlayerChoice.ToUpper() == "K" && secondPlayerChoice.ToUpper() == "N" )||(firstPlayerChoice.ToUpper() == "N" && secondPlayerChoice.ToUpper() == "P" ) ){
+          Console.WriteLine ("Gracz " + firstPlayerName + " wygrywa, gratulacje!");
+          } else {
+            Console.WriteLine ("Gracz " + secondPlayerName + " wygrywa, gratulacje!");
+            }
+          }
+        Console.WriteLine ("Czy chcecie zagrać jeszcze raz?\nNapiszcie TAK lub NIE");
+        string decision = Console.ReadLine();
+        if (decision.ToUpper() == "TAK"){
+          Console.WriteLine ("Zagrajmy jeszcze raz!");
+          continue;
+          }else{
+            game=false;
+            }
+          }
     Console.WriteLine ("PAPA!");
   }
 }
